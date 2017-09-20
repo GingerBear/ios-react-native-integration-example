062c830200a9fea660150c3c48301c4e904e53c5
GingerBear <bbbear444@gmail.com>
2017-09-20 16:15:39 -0400

    make basic react native run

--- RNWrapper2/ViewController.m
+++ RNWrapper2/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <React/RCTRootView.h>
 
 @interface ViewController ()
 
@@ -25,5 +26,30 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)highScoreButtonPressed:(id)sender {
+    NSLog(@"High Score Button Pressed");
+    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios"];
+    
+    RCTRootView *rootView =
+    [[RCTRootView alloc] initWithBundleURL: jsCodeLocation
+                                moduleName: @"MyReactNativeApp"
+                         initialProperties:
+     @{
+       @"scores" : @[
+               @{
+                   @"name" : @"Alex",
+                   @"value": @"42"
+                   },
+               @{
+                   @"name" : @"Joel",
+                   @"value": @"10"
+                   }
+               ]
+       }
+                             launchOptions: nil];
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view = rootView;
+    [self presentViewController:vc animated:YES completion:nil];
+}
 
 @end
